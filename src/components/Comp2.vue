5f80bf1287ccd282591a783470e29175eac7307e
jiawenxue0912 <1191827316@qq.com>
2018-04-22 16:40:08 +0800

    4.22

---
+++ src/components/Comp2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Comp2 html!!</h1>
+    <h1>姓名:{{$route.params.name}}</h1>
+    <h1>年龄:{{$route.params.age}}</h1>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default{
+    name:"Comp2",
+  beforeRouteEnter:(to,from,next)=>{
+    console.log('进入comp2');
+      next()
+  },
+  beforeRouteLeave:(to,from,next)=>{
+    console.log('离开comp2');
+    next()
+  }
+}
+</script>
+<style scoped>
+
+</style>
