c15c92a2c74f80ed35e8373a65dd515e0364359e
jiawenxue0912 <1191827316@qq.com>
2018-04-23 16:04:47 +0800

    4.23

---
+++ src/components/Comp4.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1> {{$route.params.name}}</h1>
+    <h1> {{$route.params.age}}</h1>
+    <router-view/>
+  </div>
+</template>
+<script>
+  export default{
+    name:"Comp4"
+  }
+</script>
+<style scoped>
+
+</style>
